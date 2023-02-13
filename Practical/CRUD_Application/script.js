082efee22563b099bf3ff70085abda751dafe996
jainam.b <jainam.b@simformsolutions.com>
2023-02-13 09:35:58 +0530

    It has functionality of Add, Show, Edit, Remove products data, Sort by price, Filter by Name and Id, Searchbar, Navbar etc.

--- Practical/CRUD_Application/script.js
+++ Practical/CRUD_Application/script.js
@@ -6,28 +6,43 @@ function validateForm() {
   var pd = document.getElementById("pd").value;
   var pp = document.getElementById("pp").value;
 
-  if (pname == "") {
-    alert("Producrt Name is required");
+
+  if (pname == "" || pp == "" || pd == "") {
+    alert("All fields are required!");
     return false;
   }
 
-  if (pid == "") {
-    alert("Producrt Identification must be specified");
+  // if (pname == "") {
+  //   alert("Product Name is required");
+  //   return false;
+  // }
+
+  // if (pid == "") {
+  //   alert("Product Identification must be specified");
+  //   return false;
+  // }
+
+  // if (pi == "") {
+  //   alert("Please upload a product image");
+  //   return false;
+  // }
+
+  // if (pd == "") {
+  //   alert("Please enter a product details");
+  //   return false;
+  // }
+
+  // if (pp == "") {
+  //   alert("Product price must be specified");
+  //   return false;
+  // }
+
+  if (isNaN(pid)) {
+    alert("Only numeric Value");
     return false;
   }
-
-  if (pi == "") {
-    alert("Please upload a product image");
-    return false;
-  }
-
-  if (pd == "") {
-    alert("Please enter a product deatils");
-    return false;
-  }
-
-  if (pp == "") {
-    alert("Product price must be specified");
+  if (pp < 0) {
+    alert("Price must be positive.");
     return false;
   }
 
@@ -48,7 +63,11 @@ function showData() {
     html += "<tr>";
     html += "<td>" + element.pname + "</td>";
     html += "<td>" + element.pid + "</td>";
-    html += "<td>" + element.pi + "</td>";
+    // html += "<td>" + element.pi + "</td>";
+    html +=
+      "<td> <img src=" +
+      element.pi +
+      "style='height:100px; width:100px;' alt='Product Image' ></td>";
     html += "<td>" + element.pd + "</td>";
     html += "<td>" + element.pp + "</td>";
     html +=
@@ -60,11 +79,9 @@ function showData() {
     html += "</tr>";
   });
   document.querySelector("#CRUDTable tbody").innerHTML = html;
-  console.log("html");
 }
 
-
-//Loads all data from local storage when document or page loded
+//Loads all data from local storage when document or page loaded
 document.onload = showData();
 
 // function to add data to local storage
@@ -73,27 +90,33 @@ function AddData() {
   if (validateForm() == true) {
     var pname = document.getElementById("pname").value;
     var pid = document.getElementById("pid").value;
-    var pi = document.getElementById("pi").value;
+    // var pi = document.getElementById("pi").value;
+    var pi = document.getElementById("pi").files[0];
     var pd = document.getElementById("pd").value;
     var pp = document.getElementById("pp").value;
 
-    var productlist;
-    if (localStorage.getItem("productlist") == null) {
-      productlist = [];
-    } else {
-      productlist = JSON.parse(localStorage.getItem("productlist"));
-    }
+    let reader = new FileReader();
+    reader.readAsDataURL(pi);
+    reader.addEventListener("load", () => {
+      var url = reader.result;
+      var productlist;
+      if (localStorage.getItem("productlist") == null) {
+        productlist = [];
+      } else {
+        productlist = JSON.parse(localStorage.getItem("productlist"));
+      }
 
-    productlist.push({
-      pname: pname,
-      pid: pid,
-      pi: pi,
-      pd: pd,
-      pp: pp,
+      productlist.push({
+        pname: pname,
+        pid: pid,
+        pi: url,
+        pd: pd,
+        pp: pp,
+      });
+      localStorage.setItem("productlist", JSON.stringify(productlist));
+      showData();
     });
 
-    localStorage.setItem("productlist", JSON.stringify(productlist));
-    showData();
     document.getElementById("pname").value = "";
     document.getElementById("pid").value = "";
     document.getElementById("pi").value = "";
@@ -121,6 +144,7 @@ function updateData(index) {
   //submit button will hide and update button will show for updating of data in local storage
   document.getElementById("submit").style.display = "none";
   document.getElementById("update").style.display = "block";
+  let newimg = document.getElementById("pi");
 
   var productlist;
   if (localStorage.getItem("productlist") == null) {
@@ -131,29 +155,167 @@ function updateData(index) {
 
   document.getElementById("pname").value = productlist[index].pname;
   document.getElementById("pid").value = productlist[index].pid;
-  //document.getElementById("pi").value = productlist[index].pi;
+  document.getElementById("imgedit").src = productlist[index].pi;
   document.getElementById("pd").value = productlist[index].pd;
   document.getElementById("pp").value = productlist[index].pp;
 
+  pid.disabled = true;
+
   document.querySelector("#update").onclick = function () {
-    if (validateForm() == true) {
-      productlist[index].pname = document.getElementById("pname").value;
-      productlist[index].pid = document.getElementById("pid").value;
-      productlist[index].pi = document.getElementById("pi").value;
-      productlist[index].pd = document.getElementById("pd").value;
-      productlist[index].pp = document.getElementById("pp").value;
+    if (newimg.value == "") {
+      if (validateForm() == true) {
+        productlist[index].pname = document.getElementById("pname").value;
+        // productlist[index].pid = document.getElementById("pid").value;
+        // productlist[index].pi = document.getElementById("pi").value;
+        productlist[index].pd = document.getElementById("pd").value;
+        productlist[index].pp = document.getElementById("pp").value;
 
-      localStorage.setItem("productlist", JSON.stringify(productlist));
-      showData();
-      document.getElementById("pname").value = "";
-      document.getElementById("pid").value = "";
-      document.getElementById("pi").value = "";
-      document.getElementById("pd").value = "";
-      document.getElementById("pp").value = "";
+        localStorage.setItem("productlist", JSON.stringify(productlist));
+        showData();
+        document.getElementById("pname").value = "";
+        document.getElementById("pid").value = "";
+        document.getElementById("pi").value = "";
+        document.getElementById("pd").value = "";
+        document.getElementById("pp").value = "";
 
+        //update button will hide and submit button will show
+        document.getElementById("submit").style.display = "block";
+        document.getElementById("update").style.display = "none";
+      }
+    } else if (validateForm() == true) {
+      let pi = newimg.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(pi);
+
+      // window.addEventListener("load", function () {
+      //   document
+      //     .querySelector('input[type="file"]')
+      //     .addEventListener("change", function () {
+      //       if (this.files && this.files[index]) {
+      //         var pi = document.querySelector("pi");
+      //         pi.src = URL.createObjectURL(this.files[index]);
+      //         pi.onload = piLoaded;
+      //       }
+      //     });
+      // });
+
+      // function piLoaded(e) {
+      //   alert(e);
+      // }
+
+      reader.addEventListener("load", () => {
+        url = reader.result;
+        productlist[index].pname = document.getElementById("pname").value;
+        productlist[index].pi = url;
+        productlist[index].pp = document.getElementById("pp").value;
+        productlist[index].pd = document.getElementById("pd").value;
+
+        localStorage.setItem("productlist", JSON.stringify(productlist));
+        showData();
+        document.getElementById("pname").value = "";
+        document.getElementById("pid").value = "";
+        document.getElementById("pi").value = "";
+        document.getElementById("pd").value = "";
+        document.getElementById("pp").value = "";
+      });
       //update button will hide and submit button will show
       document.getElementById("submit").style.display = "block";
       document.getElementById("update").style.display = "none";
     }
   };
+}
+
+//function to Sort and Filter data
+function arrangeBy(x) {
+  var productlist;
+  if (localStorage.getItem("productlist") == null) {
+    productlist = [];
+  } else {
+    productlist = JSON.parse(localStorage.getItem("productlist"));
+  }
+
+  switch (x) {
+    case "lowest":
+      productlist.sort((a, b) => {
+        return a.pp - b.pp;
+      });
+      localStorage.setItem("productlist", JSON.stringify(productlist));
+      showData();
+      break;
+
+    case "highest":
+      productlist.sort((a, b) => {
+        return b.pp - a.pp;
+      });
+      localStorage.setItem("productlist", JSON.stringify(productlist));
+      showData();
+      break;
+
+    case "1ton":
+      productlist.sort((a, b) => {
+        return a.pid - b.pid;
+      });
+      localStorage.setItem("productlist", JSON.stringify(productlist));
+      showData();
+      break;
+
+    case "atoz":
+      productlist.sort(function (a, b) {
+        var x = a.pname.toLowerCase();
+        var y = b.pname.toLowerCase();
+        return x < y ? -1 : x > y ? 1 : 0;
+      });
+      localStorage.setItem("productlist", JSON.stringify(productlist));
+      showData();
+      break;
+  }
+}
+
+//Function to validate Image
+function validateImage() {
+  var pi = document.getElementById("pi");
+  var file = pi.value;
+  var extensions = /(\.webp|\.jpg|\.jpeg|\.svg|\.png|\.gif)$/i;
+
+  if (!extensions.exec(file)) {
+    alert("Invalid file type");
+    fileInput.value = "";
+    return false;
+  }
+  return true;
+}
+
+//Function to display image
+function readURL(input) {
+  document.getElementById("pi").src = "";
+  if (input.files || input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#imgedit").attr("src", e.target.result).width(100).height(100);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+//Function to search by id in search bar
+function searchById() {
+  var input, filter, table, tr, td, i, txtValue, j;
+  input = document.getElementById("search-input");
+  filter = input.value;
+  table = document.getElementById("CRUDTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    for (j = 0; j <= i; j++) {
+      td = tr[j].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[j].style.display = "";
+        } else {
+          tr[j].style.display = "none";
+        }
+      }
+    }
+  }
 }
